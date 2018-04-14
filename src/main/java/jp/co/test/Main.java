e7de0778d0e301fd5f4927f4a7c603480c068f18
tosashimanto <tosashimanto@gmail.com>
2018-04-14 20:23:54 +0900

    initial commit.

---
+++ src/main/java/jp/co/test/Main.java
@@ -0,0 +1,10 @@
+package jp.co.test;
+
+import org.springframework.boot.SpringApplication;
+
+public class Main {
+    public static void main(String [] args) {
+        System.exit(SpringApplication.exit(SpringApplication.run(
+                BatchConfiguration.class, args)));
+    }
+}
